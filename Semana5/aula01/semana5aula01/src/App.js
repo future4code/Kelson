b9e06e3b2f3226be1743c5c484da3ad03c46ad0c
Kelson <kelsoncarmozine@globo.com>
2019-10-07 15:00:08 -0300

    Parcial - Semana 5 aula 01

---
+++ Semana5/aula01/semana5aula01/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = { valuetask : ''}
+  }
+  render(){
+      return (
+        <div className="App">
+        
+        </div>
+      );
+    }
+}
+
+export default App;
