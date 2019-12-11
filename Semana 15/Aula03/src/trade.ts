d5522d3feb4969e4ccbc267a1c85d52103fa8a47
Kelson <kelsoncarmozine@globo.com>
2019-12-11 12:50:56 -0300

    Exercicio semna15 aula03 - Final

---
+++ Semana 15/Aula03/src/trade.ts
@@ -0,0 +1,14 @@
+import { Place } from "./place";
+
+export class Trade extends Place {
+  constructor(
+    public name: string,
+    // Refere-se ao nome da indústria
+
+    public cnpj: string, // Refere-se ao número de registro da empresa
+    cep: string
+  ) {
+    super(cep);
+  }
+}
+// é uma classe filha da classe Place
